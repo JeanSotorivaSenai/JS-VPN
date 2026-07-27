66c1c3ab3dbd6d0ef462873c727cd626c52d19a1
jean.sotoriva <jean.sotoriva@sc.senai.br>
2026-07-27 11:03:14 -0300

    [v. 1.0] - Att para abrir o App com click esquerdo ícone barra de tarefas e menu com direito. (Execute update conforme readme).

--- js_vpn/tray.py
+++ js_vpn/tray.py
@@ -2,10 +2,9 @@ from __future__ import annotations
 
 import gi
 
-gi.require_version("AyatanaAppIndicator3", "0.1")
 gi.require_version("Gtk", "3.0")
 
-from gi.repository import AyatanaAppIndicator3, GLib, Gtk
+from gi.repository import GLib, Gtk
 
 from .vpn import disconnect, is_connected
 
@@ -14,45 +13,63 @@ class TrayIndicator:
     def __init__(self, window: Gtk.Window) -> None:
         self.window = window
 
-        self.indicator = AyatanaAppIndicator3.Indicator.new(
-            "vpn-js",
-            "network-vpn-disconnected",
-            AyatanaAppIndicator3.IndicatorCategory.APPLICATION_STATUS,
+        self.tray_icon = Gtk.StatusIcon.new_from_icon_name(
+            "network-vpn-disconnected"
         )
-        self.indicator.set_status(
-            AyatanaAppIndicator3.IndicatorStatus.ACTIVE
-        )
+        self.tray_icon.set_tooltip_text("JS VPN")
+        self.tray_icon.set_visible(True)
+
+        self.menu = Gtk.Menu()
 
         self.status_item = Gtk.MenuItem(label="VPN desconectada")
         self.status_item.set_sensitive(False)
+        self.menu.append(self.status_item)
 
-        menu = Gtk.Menu()
-        menu.append(self.status_item)
-        menu.append(Gtk.SeparatorMenuItem())
+        self.menu.append(Gtk.SeparatorMenuItem())
 
         open_item = Gtk.MenuItem(label="Abrir JS VPN")
         open_item.connect("activate", self.show_window)
-        menu.append(open_item)
+        self.menu.append(open_item)
 
-        disconnect_item = Gtk.MenuItem(label="Desconectar")
-        disconnect_item.connect("activate", self.disconnect_vpn)
-        menu.append(disconnect_item)
-        self.disconnect_item = disconnect_item
+        self.disconnect_item = Gtk.MenuItem(label="Desconectar")
+        self.disconnect_item.connect("activate", self.disconnect_vpn)
+        self.menu.append(self.disconnect_item)
 
-        menu.append(Gtk.SeparatorMenuItem())
+        self.menu.append(Gtk.SeparatorMenuItem())
 
         quit_item = Gtk.MenuItem(label="Sair")
         quit_item.connect("activate", self.quit_application)
-        menu.append(quit_item)
+        self.menu.append(quit_item)
 
-        menu.show_all()
-        self.indicator.set_menu(menu)
+        self.menu.show_all()
+
+        # Clique esquerdo.
+        self.tray_icon.connect("activate", self.show_window)
+
+        # Clique direito.
+        self.tray_icon.connect("popup-menu", self.show_context_menu)
 
         self.refresh_status()
         GLib.timeout_add_seconds(2, self.refresh_status)
 
+    def show_context_menu(
+        self,
+        icon: Gtk.StatusIcon,
+        button: int,
+        activate_time: int,
+    ) -> None:
+        self.menu.popup(
+            None,
+            None,
+            Gtk.StatusIcon.position_menu,
+            icon,
+            button,
+            activate_time,
+        )
+
     def show_window(self, _item=None) -> None:
         self.window.show_all()
+        self.window.deiconify()
         self.window.present()
 
     def disconnect_vpn(self, _item=None) -> None:
@@ -66,17 +83,15 @@ class TrayIndicator:
         connected = is_connected()
 
         if connected:
-            self.indicator.set_icon_full(
-                "network-vpn",
-                "VPN conectada",
-            )
+            self.tray_icon.set_from_icon_name("network-vpn")
+            self.tray_icon.set_tooltip_text("JS VPN — conectada")
             self.status_item.set_label("VPN conectada")
             self.disconnect_item.set_sensitive(True)
         else:
-            self.indicator.set_icon_full(
-                "network-vpn-disconnected",
-                "VPN desconectada",
+            self.tray_icon.set_from_icon_name(
+                "network-vpn-disconnected"
             )
+            self.tray_icon.set_tooltip_text("JS VPN — desconectada")
             self.status_item.set_label("VPN desconectada")
             self.disconnect_item.set_sensitive(False)
 
